639710dc23e63d658eac0832d2ad522764919fda
Aleksandr <omelchenkoaleks@gmail.com>
2026-01-13 08:10:39 +0200

    style: all screens with text and color styles

--- lib/features/manager/presentation/tracking/tracking_screen.dart
+++ lib/features/manager/presentation/tracking/tracking_screen.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:texa_core/core/extensions/theme_ext.dart';
+import 'package:texa_core/core/l10n/gen/app_localizations.dart';
+import 'package:texa_core/core/theme/app_sizes.dart';
+import 'package:texa_core/core/theme/app_typography.dart';
 
 class TrackingScreen extends StatelessWidget {
   const TrackingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final colors = context.color;
+    final localizations = AppLocalizations.of(context);
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Tracking')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [const Text('Tracking Screen')],
+      backgroundColor: colors.background,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: colors.surface,
+        elevation: 0,
+        scrolledUnderElevation: 0,
+        centerTitle: false,
+        titleSpacing: AppSizes.medium,
+        title: Text(
+          localizations.tracking,
+          style: AppTypography.h2.copyWith(color: colors.textPrimary),
         ),
+
+        // actions: [
+        //   Padding(
+        //     padding: AppSizes.medium.rightPadding,
+        //     child: IconButton(
+        //       onPressed: () {},
+        //       icon: Icon(
+        //         Icons.notifications_none_rounded,
+        //         color: colors.textPrimary,
+        //       ),
+        //     ),
+        //   ),
+        // ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1.0),
+          child: Container(color: colors.borderLight, height: 1.0),
+        ),
+      ),
+
+      body: Center(
+        child: Text('Tracking', style: AppTypography.bodyMediumBold),
       ),
     );
   }
